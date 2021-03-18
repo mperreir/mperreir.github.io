c15589e2f75f79bb6ab529b5e822ef9cb5b4d688
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2021-03-18 16:02:05 +0100

    Mise à jour CSS + contenu 2021

---
+++ technosweb/generate-pdf.sh
@@ -0,0 +1,6 @@
+decktape remark http://127.0.0.1:8000/intro.html pdf/00-intro.pdf
+decktape remark http://127.0.0.1:8000/html.html pdf/01-html.pdf
+decktape remark http://127.0.0.1:8000/css.html pdf/02-css.pdf
+decktape remark http://127.0.0.1:8000/protocoles.html pdf/03-protocoles.pdf
+decktape remark http://127.0.0.1:8000/prog-serveur.html pdf/04-prog-serveur.pdf
+decktape remark http://127.0.0.1:8000/services-web.html pdf/05-services-web.pdf
