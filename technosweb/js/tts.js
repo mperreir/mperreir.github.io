66b9bb5f2286ec290f7fedc1b19db7eaa4ac7f8b
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2022-03-24 11:34:10 +0100

    Ajout du text to speech

---
+++ technosweb/js/tts.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const theSpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new theSpeechRecognition();
+
+const initTTS = function() {
+    let subtitle = ['div.transcript'].toDOM(document.querySelector('body')); 
+    subtitle.setAttribute('style', 'visibility: hidden;');
+    recognition.lang = 'FR_fr';
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.onresult = function(event) {
+        //console.log(event);
+        //console.log(event.resultIndex, [...event.results].map(srr => [...srr].map(sra => sra.transcript).join('/') + srr.isFinal));
+        subtitle = ['div.transcript',
+            ...[...event.results].slice(event.resultIndex).map(srr => ['p', srr.isFinal ? {} : {class: 'interim'}, [...srr].map(sra => sra.transcript).join('/')]),
+        ].toDOM({replace: subtitle});
+    }
+    //recognition.start();
+    let started = false;
+    document.addEventListener('keydown', (event) => {
+        const keyPressed = event.key;
+
+        if( keyPressed == 't' ){
+            if (started) {
+                recognition.stop();
+                started = false;
+                subtitle = ['div.transcript'].toDOM({replace: subtitle});
+                subtitle.setAttribute('style', 'visibility: hidden;');
+            }
+            else {
+                recognition.start();
+                started = true;
+                subtitle.setAttribute('style', 'visibility: visible;');
+            }
+            console.log('Speech-to-text: ', started);
+        }
+    });
+}
