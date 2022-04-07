70ac98efbecf1fbe0d47a19b7f559becc331a698
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2022-04-07 11:24:11 +0200

    Ajout des bonus (à finir)

--- technosweb/generate-pdf.sh
+++ technosweb/generate-pdf.sh
@@ -4,3 +4,5 @@ decktape remark http://127.0.0.1:8000/css.html pdf/02-css.pdf
 decktape remark http://127.0.0.1:8000/protocoles.html pdf/03-protocoles.pdf
 decktape remark http://127.0.0.1:8000/prog-serveur.html pdf/04-prog-serveur.pdf
 decktape remark http://127.0.0.1:8000/services-web.html pdf/05-services-web.pdf
+decktape remark http://127.0.0.1:8000/dom.html pdf/06-dom.pdf
+decktape remark http://127.0.0.1:8000/mustache.html pdf/07-mustache.pdf
